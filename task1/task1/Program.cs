56aa2d7ca2ec73d9c109a96bd7d13206c825b78f
maxembo <91334358+maxembo@users.noreply.github.com>
2023-03-20 17:21:20 +0300

    Add project files.

---
+++ task1/task1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace  task1
+{
+    internal class Program
+    {
+   
+        private static int[,] RectangleMatrix(int rows, int cols)
+        {
+            int[,] matrix = new int[rows,cols];
+
+            if (rows != cols)
+                return matrix;
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    bool isDiagonal = (i == j);
+                    bool isAboveDiagonal = (i < j);
+                    bool isBelowDiagonal = (i > j);
+                    
+                    if (isDiagonal)
+                        matrix[i, j] = 1;
+                    else if(isAboveDiagonal)
+                        matrix[i, j] = i + j;
+                    else if (isBelowDiagonal)
+                        matrix[i, j] = i - j;
+                }
+            }
+            return matrix;
+        }
+        
+        private static void PrintMatrix(int[,] matrix)
+        {
+            Console.WriteLine("\nКвадратная матрица:\n");
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i,j] + " ");
+                } 
+                Console.WriteLine();
+            }
+        }
+        private static void Main(String[] args)
+        { 
+            Console.Write("Введите число строк: ");
+            int rows = int.Parse(Console.ReadLine());
+            
+            Console.Write("\nВведите число столбцов: ");
+            int cols = int.Parse(Console.ReadLine());
+            
+            PrintMatrix(RectangleMatrix(rows,cols));
+        }
+    }
+}
---
+++ task2/task2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace task2
+{
+    internal class Program
+    {
+        private static void SplitText(string Name0, int k, string Name1, string Name2, string Name3)
+        {
+            using (StreamReader streamReader = new StreamReader(Name0))
+            using (StreamWriter streamWriter1 = new StreamWriter(Name1))
+            using (StreamWriter streamWriter2 = new StreamWriter(Name2))
+            using(StreamWriter streamWriter3 = new StreamWriter(Name3))
+            {
+                string s1 = "";
+                string rows = "";
+                for (int i = 0; i < k; i++)
+                {
+                    s1 = streamReader.ReadLine();
+                    
+                    streamWriter1.WriteLine(s1);
+                    streamWriter3.WriteLine(s1);
+                }
+                
+                while (!streamReader.EndOfStream)
+                {
+                    rows =  streamReader.ReadLine();
+                    
+                    streamWriter2.WriteLine(rows); 
+                    streamWriter3.WriteLine(rows); 
+                }
+            }
+        }
+        
+        private static void Main(String[] args)
+        {
+            SplitText("Name0.txt", 3, "Name1.txt","Name2.txt","Name3.txt");
+        }
+    }
+}
